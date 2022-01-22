331a970c99eb52da8f56d57b2c9631954d91581d
i-chernyshov <chernyshov629@gmail.com>
2022-01-22 21:31:51 +0300

    Add type orm module, docker compose for pg and topics resource.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TopicsModule } from './topics/topics.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), TopicsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/topics/topics.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateTopicDto } from './dto/create-topic.dto';
+import { UpdateTopicDto } from './dto/update-topic.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Topic } from './entities/topic.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TopicsService {
+  constructor(
+    @InjectRepository(Topic)
+    private readonly topicRepository: Repository<Topic>,
+  ) {}
+
+  create(createTopicDto: CreateTopicDto) {
+    const topic = this.topicRepository.create(createTopicDto);
+    return this.topicRepository.save(topic);
+  }
+
+  findAll() {
+    return this.topicRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.topicRepository.findOneOrFail(id);
+  }
+
+  async update(id: number, updateTopicDto: UpdateTopicDto) {
+    const topic = await this.topicRepository.preload({
+      id,
+      ...updateTopicDto,
+    });
+    if (!topic) {
+      throw new NotFoundException(`topic ${id} not found`);
+    }
+    return this.topicRepository.save(topic);
+  }
+
+  async remove(id: number) {
+    const topic = await this.topicRepository.findOneOrFail(id);
+    return this.topicRepository.remove(topic);
+  }
+}
